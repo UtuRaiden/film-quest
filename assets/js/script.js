0c25e2768ecccd9062b8066c45df5c8b9f76ecac
Diego Borjas <diegoborjas@gmail.com>
2023-04-29 12:58:52 -0500

    updated header and edited html title

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,7 @@ var searchAreaEl = document.querySelector('#searchArea')
 var searchInputEl = document.querySelector('#searchInput')
 var searchBtnEl = document.querySelector('#searchBtn')
 
-
-
 var MDB_BASE = 'https://api.themoviedb.org/3/search/movie?api_key=a1ad9f4fd19c47b55d47d59ffc20d5bc&query='
-
-
-
-
-
 
 searchAreaEl.addEventListener('click',function(event){
     var element = event.target
@@ -20,7 +13,6 @@ searchAreaEl.addEventListener('click',function(event){
     var MDB_SEARCH = MDB_BASE + input
     getMovieResults(MDB_SEARCH)
 })
-
 
 function getMovieResults(Movie){
 fetch(Movie)
